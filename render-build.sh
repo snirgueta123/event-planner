2eec218a63af5cc26960204bcf3cee143ea735a8
snirgueta123 <snir425@gmail.com>
2025-06-22 23:09:34 +0300

    Fix CORS config

--- render-build.sh
+++ render-build.sh
@@ -1,4 +1,13 @@
-#!/usr/bin/env bash
-python backend/event_ticketing_system/manage.py collectstatic --noinput
-python backend/event_ticketing_system/manage.py migrate
+#!/bin/bash
 
+# נכנסים לתיקיית backend כי שם נמצא Django project
+cd backend
+
+# מתקינים את התלויות
+pip install -r requirements.txt
+
+# מבצעים את ה-collectstatic בלי קלט משתמש
+python manage.py collectstatic --noinput
+
+# מריצים מיגרציות בסיס הנתונים
+python manage.py migrate
