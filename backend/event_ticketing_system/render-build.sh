a4d5a71f5dd7a16532413e1ad35ac69b813597bf
snirgueta123 <snir425@gmail.com>
2025-06-29 16:08:44 +0300

    Move render-build.sh into backend/event_ticketing_system

--- backend/event_ticketing_system/render-build.sh
+++ backend/event_ticketing_system/render-build.sh
@@ -1,7 +1,10 @@
 #!/bin/bash
 
 echo "📦 Installing dependencies..."
-pip install -r ../requirements.txt
+pip install -r backend/requirements.txt
+
+echo "⏳ Changing directory to backend/event_ticketing_system"
+cd backend/event_ticketing_system || exit 1
 
 echo "🧹 Collecting static files..."
 python manage.py collectstatic --noinput
